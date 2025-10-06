05345d5e529d0f6a4e32ab96bf7d2375487d6e1d
GabrielGareca <garecagabriel1@gmail.com>
2025-10-06 15:10:36 -0300

    agregar casurbia gabi

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
+import CasurbiaGabi from "~/Components/casurbiaGabi";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <CasurbiaGabi />;
 }
